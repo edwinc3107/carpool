1a2e140ac97e451f6ff989bcdc9867a7c8ac38e4
Edwin Clatus <officialedwn@gmail.com>
2026-03-09 00:36:47 -0400

    Use VITE_API_URL for Socket.IO for production

--- myapp/src/socket.js
+++ myapp/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000", {
+const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+const socket = io(SOCKET_URL, {
   withCredentials: true,
 });
 
